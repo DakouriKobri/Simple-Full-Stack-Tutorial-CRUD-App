a2ea7c641fca04bc0a3bb841e17596d39fc33ab6
Dakouri  Kobri <maurille.kobri@gmail.com>
2021-04-22 10:24:39 +0200

    Model Tutorial entity

---
+++ src/main/java/com/dakourikobri/Tutorial.java
@@ -0,0 +1,57 @@
+package com.dakourikobri;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private String published;
+
+    public Tutorial() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+}
+
